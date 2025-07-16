1f3d11b3e8a76878ae03b646ece2a6e5795b427f
Jackson <kahongejackson7@gmail.com>
2025-07-17 00:18:12 +0300

    Initialize frontend

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css';
+
+function App() {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl">Film Fanatics</h1>
+    </div>
+  );
+}
+
+export default App;
