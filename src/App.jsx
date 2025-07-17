b94ff2508a1fc6ac265e7e21aadd687aff8937b1
KellyBrianMurimi <kellybrianmurimi@gmail.com>
2025-07-17 11:42:58 +0300

    Index

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
-import React from 'react';
-import './index.css';
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Layout/Navbar'
+import Login from './components/Auth/Login'
+import Register from './components/Auth/Register'
+import ClubLists from './components/Clubs/ClubLists'
+import CreateClub from './components/Clubs/CreateClub'
+import './index.css'
 
 function App() {
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-2xl">Film Fanatics</h1>
-    </div>
-  );
+    <Router>
+      <Navbar />
+      <div className="container">
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/clubs" element={<ClubLists />} />
+          <Route path="/clubs/create" element={<CreateClub />} />
+          <Route path="/" element={<h1>Welcome to Film Fanatics</h1>} />
+        </Routes>
+      </div>
+    </Router>
+  )
 }
 
-export default App;
+export default App
