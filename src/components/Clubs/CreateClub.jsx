ce7bdad5b3579065027a8dff1635e51b85a92a52
KellyBrianMurimi <kellybrianmurimi@gmail.com>
2025-07-21 19:02:10 +0300

    update

--- src/components/Clubs/CreateClub.jsx
+++ src/components/Clubs/CreateClub.jsx
@@ -62,4 +62,3 @@ const CreateClub = () => {
 };
 
 export default CreateClub;
-
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { fileURLToPath, URL } from 'url';
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: 5173,
   },
-  esbuild: {
-    loader: { '.js': 'jsx' },
-  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 });
