c64c3a938c1e7807e490c654a85ab622fea7b506
Andrew <andrewmandieka@gmail.com>
2025-08-01 17:58:59 +0300

    updated pdf file and pptx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+-import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/Layout/Navbar';
 import Login from './components/Auth/Login';
